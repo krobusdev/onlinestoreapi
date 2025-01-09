04ee39f6e237170beba2aaa088fe61334c280e56
krobusdev <it5myemail@ya.ru>
2025-01-09 22:25:37 +0300

    updated for Docker

--- adapters/database.py
+++ adapters/database.py
@@ -3,13 +3,20 @@ from fastapi import HTTPException
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:121212@localhost:5432/onlinestore"
+from domain.gameModel import Base
+
+
+#DATABASE_URL = "postgresql://postgres:121212@localhost:5432/onlinestore"
+DATABASE_URL = "postgresql://postgres:121212@postgres-db:5432/onlinestore"
 
 engine = create_engine(DATABASE_URL)
+
+#SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
     db = SessionLocal()
+    Base.metadata.create_all(bind=engine)
     try:
         yield db
     except:
